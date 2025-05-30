16bf2e843177e2a047237a8505618ea3ee0f36c7
z <>
2025-05-31 00:01:41 +0800

    glide cache maximum 10MB

--- app/src/main/java/com/fongmi/android/tv/utils/OkHttpGlideModule.java
+++ app/src/main/java/com/fongmi/android/tv/utils/OkHttpGlideModule.java
@@ -6,9 +6,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 import com.github.catvod.net.OkHttp;
@@ -21,5 +23,12 @@ public class OkHttpGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(@NonNull Context context, @Nullable Glide glide, Registry registry) {
         registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(OkHttp.client()));
+    }
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        // 设置磁盘缓存大小为 10MB
+        int diskCacheSizeBytes = 1024 * 1024 * 10;
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, diskCacheSizeBytes));
     }
 }
